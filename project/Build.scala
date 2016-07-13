06b47bef99eb6e9cf07c9a3d99969e7b5eb13aa5
Koert Kuipers <koert@tresata.com>
2016-07-13 09:33:33 -0400

    release 6.30.0-tres2

--- project/Build.scala
+++ project/Build.scala
@@ -8,7 +8,7 @@ object ProjectBuild extends Build {
     settings = Project.defaultSettings ++ Seq(
       organization := "com.twitter",
       name := "util-eval",
-      version := "6.30.0-tres2-SNAPSHOT",
+      version := "6.30.0-tres2",
       scalaVersion := "2.11.8",
       libraryDependencies <++= (scalaVersion) { scalaVersion => Seq(
         "org.scala-lang" % "scala-compiler" % scalaVersion % "compile",
